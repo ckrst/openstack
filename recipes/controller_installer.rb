a7f22fc1357738c7d2a4ba99042e55ba3d83ae9d
Vinícius Kirst <vinicius@versul.com.br>
2016-07-13 18:09:06 -0300

    networks

--- recipes/controller_installer.rb
+++ recipes/controller_installer.rb
@@ -389,3 +389,38 @@ end
 service 'apache2' do
     action [ :reload ]
 end
+
+execute 'provider_network' do
+    command "neutron net-create --shared --provider:physical_network provider --provider:network_type flat provider"
+    environment admin_env
+end
+
+execute 'provider_subnetwork' do
+    command "neutron subnet-create --name provider --allocation-pool start=10.40.199.20,end=10.40.199.199 --dns-nameserver 8.8.8.8 --gateway 10.40.0.1 provider 10.40.0.0/16"
+    environment admin_env
+end
+
+execute 'selfservice_network' do
+    command "neutron net-create selfservice"
+    environment admin_env
+end
+
+execute 'selfservice_subnetwork' do
+    command "neutron subnet-create --name selfservice --dns-nameserver 8.8.8.8 --gateway 172.16.1.1 selfservice 172.16.1.0/24"
+    environment admin_env
+end
+
+execute 'create_router' do
+    command "neutron router-create router"
+    environment admin_env
+end
+
+execute 'add_ssnetwork_subnet_as_router_interface' do
+    command "neutron router-interface-add router selfservice"
+    environment admin_env
+end
+
+execute 'set_gateway_provider_network' do
+    command "neutron router-gateway-set router provider"
+    environment admin_env
+end
